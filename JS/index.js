9c239c70b53137507c5d235c8ca235293dd890ba
Alex Pliakas <pliakasbenitez@gmail.com>
2020-10-29 01:55:59 +0100

    some things work

--- JS/index.js
+++ JS/index.js
@@ -12,143 +12,111 @@ window.addEventListener('load' ,() => {
     const holes = document.querySelectorAll('.hole');
     const howToPlay = document.getElementsByClassName("how_to_play")
     const lives = document.getElementsByClassName("noOfLives");
-    const gameTime = 60000;
+    const timer = document.getElementById('timeseconds')
+
+    let timing = 60;
+    let molesTimeout;
+    
     
    // console.log("gamelevel",gameLevel.innerHTML)  
-    let lastHole;
-    let timeUp = false;
+    let started = false;
     let score = 0;
     let level = 1;
-    console.log(startButton);
+
+    const levelTiming = () => {
+        let difficulty = parseInt(gameLevel.innerText);
+
+        switch(difficulty){
+            case 1 :
+                time = 2500;
+                break;
+            case 2 :
+                time = 2000;
+                break;
+            case 3 :
+                time = 1500;
+                break;
+            case 4 :
+                time = 1000;
+                break;
+        }
+
+        return time;
+    }
+
+    const levelTime = levelTiming();
+
     startButton.addEventListener('click', function () {
-        startGame();
-        showBtnAnimation();
-       console.log("test");
-        
+        if (started === false) {
+            startGame();
+        }
     });
- 
-    function showBtnAnimation() {
-        event.preventDefault(); 
 
-        startButton.classList.add('started');
-        
-        setTimeout(() => {
-            startButton.classList.remove('started');
-           // startButton.style.display = 'none'; 
-        }, gameTime);
-    }
-
-    function peep() {
-        const peepTime = whackedTheMole.speed(gameLevel);
-        const hole = whackedTheMole.randomHole(holes);
-        comeOutAndStopMole(hole, peepTime);
-    }
-
-    function comeOutAndStopMole(hole, peepTime) {
-        // Write the hamster out of the hole and stay for the corresponding time. If the game time is not over (timeUp), continue to exit the hole (peep).
-         hole.classList.remove('hidden');
-         hole.classList.add('mole')
-         setTimeout(() => {
-             hole.classList.add('hidden');
-             hole.classList.remove('mole');
-        if(!whackedTheMole.gameIsFinished()){
-        	peep();
-        }
-      },peepTime);
-    }
-
-    /*function comeOutAndStopJoker(hole, peepTime) {
-        // Write the hamster out of the hole and stay for the corresponding time. If the game time is not over (timeUp), continue to exit the hole (peep).
-         hole.classList.remove('hidden');
-         hole.classList.add('joker')
-         setTimeout(() => {
-             hole.classList.add('hidden');
-             hole.classList.remove('joker');
-        if(!whackedTheMole.gameIsFinished()){
-        	peep();
-        }
-      },peepTime);
-    }*/
-
-
-    function startGame() {
+    const startGame = () => {
+        started = true;
         resetScoreAndTime();
-        peep();
+        moleTime();
         
-        setTimeout(() => {
-            //  Write what will happen when the game time is over
-            timeUp = true;
-            //titleH1.innerHTML="TIME UP！";
-            startButton.style.display='inline';
-            startButton.innerHTML="Replay！";
-            scoreBoard.innerHTML=score;
-        }, gameTime)
+        let chrono = setInterval(() => {
+            timing--;
+            refreshNumbers();
+
+            if (timing === 0) {
+                clearTimeout(chrono)
+                endGame();
+            }
+        }, 1000);
     }
 
-    /**
-     * Initialize settings.
-     */
-    function resetScoreAndTime() {
+    const resetScoreAndTime = () => {
         // Write the initial settings of the game
         gameLevel.innerHTML = 1;
         scoreBoard.innerHTML = 0; 
-        timeUp = false;
         score = 0;
-        gameTime;
+        level = 1;
     }
-    /**
-     *Hit the mole. Add a click event for each mole, the score will show +1 after clicking, and the mole enters the hole
-     */
-    moles.forEach(mole => mole.addEventListener('click', function (e) {
-        //  Write here what happened when the user clicked the mole.
-        	console.log(e);
-    	if(!timeUp){
-            score ++;
-            if ( score === 10){
-                level = 2;
-            }else if(score === 20){
-                level = 3;
-            }else if ( score === 30){
-                level = 4;
-            }
-            gameLevel.innerHTML = level;
-           
-            scoreBoard.innerHTML = score;
-            
-            console.log(score);
-            
-        }   
+
+    const moleTime = () => {
+        moles.forEach((button) => {
+            button.classList.add('hidden');
+            button.classList.remove('mole')
+        });
+
+        clearInterval(molesTimeout);
         
+        setTimeout(() => {
+            showMole();
+
+            molesTimeout = setInterval(() => {
+                moleTime();
+    
+            }, levelTime);
+
+        }, Math.floor(Math.random() * 500) + 250);
         
-    }));
-
-    //Click event listener for how to play button
-    /*howToPlay.addEventListener('click', function myFunction() {
-            var popup = document.getElementsByClassName("how_to_play");
-            popup.classList.toggle("show");
-          
-    })*/
-
-
-    joker.forEach(joker => joker.addEventListener('click', function (e) {
-        //  Write here what happened when the user clicked the joker.
-        	console.log(e);
-    	if(!timeUp){
-            lives --;
-            life.innerHTML=lives
-            
-    	}    	
-    }));
-
+    };
 
     
 
+    const showMole = () => {
+        activeMole = document.getElementById(`hole${Math.floor(Math.random() * 9) + 1}`);
+        activeMole.classList.remove('hidden');
+        activeMole.classList.add('mole');
+    };
+    
 
+    moles.forEach(mole => mole.addEventListener('click', function () {
+        score++;
+        refreshNumbers();
+        moleTime();
+        
+    }))
 
+    const refreshNumbers = () => {
+        timer.innerHTML = timing;
+        scoreBoard.innerHTML = score;
+    }
 
+        
 });
-   
-/* const whacktheMole = new WhackedTheMole();
-whacktheMole.gameIsFinished(lives,timeUp);
-const whackTheJoker = new WhacktheJoker();
-whackTheJoker.test(); */
+
